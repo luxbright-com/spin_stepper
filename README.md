b527c9976b4716d6064263cb5d31153470af868b
Ulrik Södergren <ulrik@digitalfotografen.se>
2024-09-05 13:35:27 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+This library has been tested on a Raspberry Pi 4 with Raspbian Bookworm.
+
+This is a fork from the st-spin package https://github.com/m-laniakea/st_spin by eir.
---
+++ src/spin_stepper/__init__.py
@@ -0,0 +1,6 @@
+from .spin_device import Direction
+from .spin_device import SpinDevice
+from .spin_device import Command
+from .spin_device import Register
+from .spin_device import SpinStatus
+from .spin_chain import SpinChain
